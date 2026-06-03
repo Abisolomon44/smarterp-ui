61f25626855f39b87795e7b2d34ea46cacdc8691
abi <abisheela88@gmail.com>
2026-06-03 07:02:57 +0530

    UI Updated the user component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,27 +1,70 @@
 import { Routes } from '@angular/router';
+
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
-import { DashboradComponent } from './pages/dashborad/dashborad.component';
-import { DefaultLayoutComponent } from './layout/default-layout.component';
-import { CreateUserComponent } from './pages/users/create-user.component';
 import { PlansComponent } from './pages/plans/plans.component';
-
+import { DefaultLayoutComponent } from './layout/default-layout.component';
+import { DashboradComponent } from './pages/dashborad/dashborad.component';
+import { CreateUserComponent } from './pages/users/create-user.component';
+import{WorkspaceComponent} from './pages/administration/workspace/workspace.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'plans', component: PlansComponent },
-
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
 
   {
-    path: 'dashboard',
-    component: DefaultLayoutComponent,
-    children: [
-      { path: '', component: DashboradComponent },
-      { path: 'users/create', component: CreateUserComponent },
-    ],
+    path: 'login',
+    component: LoginComponent,
   },
-  { path: '**', redirectTo: 'login' },
+
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+
+  {
+    path: 'plans',
+    component: PlansComponent,
+  },
+
+ {
+  path: 'dashboard',
+  component: DefaultLayoutComponent,
+  children: [
+
+    {
+      path: '',
+      component: DashboradComponent
+    },
+
+    {
+      path: 'administration',
+      children: [
+
+        {
+          path: '',
+          component: WorkspaceComponent
+        },
+
+  
+
+        {
+          path: 'users/create',
+          component: CreateUserComponent
+        },
+
+
+      ]
+    }
+
+  ]
+},
+
+  {
+    path: '**',
+    redirectTo: 'login',
+  },
 ];
